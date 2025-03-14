28ef49da566130ff25f07ee9b8081fa251b436be
tizenharmadika <77439470+hatarvedo@users.noreply.github.com>
2025-03-14 09:57:07 +0100

    Edited python to be more closely related to current code

--- python magic (credit to nowa)/shitass.py
+++ python magic (credit to nowa)/shitass.py
@@ -5,6 +5,6 @@ with open('SearchWastePoints.json', 'r', encoding="utf-8") as f:
     for trashmachine in d:
        data.append(f'INSERT INTO machines(fillpercentage,zipcode,machineaddress) VALUES({random.randint(0,100)},{trashmachine['zipCode']},"{trashmachine['address']}");\n')
        if not any(f"VALUES({trashmachine['zipCode']}" in machine for machine in data):
-            data.append(f'INSERT INTO squattage(zipcode,squattagename) VALUES({trashmachine['zipCode']},"{trashmachine['city']}");\n')
+            data.append(f'INSERT INTO settlements(zipcode,settlementname) VALUES({trashmachine['zipCode']},"{trashmachine['city']}");\n')
 with open('output.sql', 'w', encoding="utf-8") as f:
     f.writelines(sorted(data, key=len))
